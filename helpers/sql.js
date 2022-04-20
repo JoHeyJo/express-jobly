9a0649a8a5b42419327c919df59d28ca43af09f2
jf <jpf0628@gmail.com>
2022-04-20 13:48:04 -0700

    WIP: companies

---
+++ helpers/sql.js
@@ -0,0 +1,34 @@
+const { BadRequestError } = require("../expressError");
+
+// THIS NEEDS SOME GREAT DOCUMENTATION.
+
+/** Takes in two objects: 
+ * - dataToUpdate = user data
+ * - jsToSql = camelCase to snake_case converter
+ * Takes camelCase keys of object and converts them to snake_case to match SQL db
+ * Returns an object  
+ * {
+ *                        setCols : '"first_name"=$1, "age"=$2'
+ *                        values: [ Bruce, 36 ]
+ * }
+ * 
+ */
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  //keys=[firstName, lastName, password, email, isAdmin]
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  const cols = keys.map((colName, idx) =>
+    //col=first_name, last_name, age
+      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
